238b5c7d2184e47bc643ecd1ce2ccd5c6893feed
Alex Lerikos <alerikos@gmail.com>
2019-05-30 11:22:16 -0700

    Initial commit

---
+++ Projector/ProjectorTests/ProjectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProjectorTests.swift
+//  ProjectorTests
+//
+//  Created by Alex Lerikos on 5/30/19.
+//  Copyright © 2019 kosdesigns. All rights reserved.
+//
+
+import XCTest
+@testable import Projector
+
+class ProjectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
